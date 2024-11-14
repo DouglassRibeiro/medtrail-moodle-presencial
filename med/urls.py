30bef915c47f5ebd32f83cbc23f584d07bdc96c1
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2024-11-13 21:08:15 -0300

    Configurando header da pagina, por enquanto o home

--- med/urls.py
+++ med/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/', views.homepage),
+    path('homepage/', views.homepage, name='homepage'),
     path('homepage_register/', views.homepage_register, name='homepage_register'),
 ]
