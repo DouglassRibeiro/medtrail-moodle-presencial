900289ca078bfee2d05b3cf2420bf5c2252de9bd
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2024-11-27 13:04:23 -0300

    Paginas de erros assim como novas urls

--- med/models.py
+++ med/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class CustomUser(AbstractBaseUser):
+    telefone = models.CharField(max_length=11, null=True, blank=True)
+    endereco = models.TextField(null=True, blank=True)
+
+class RegistroCalendario(models.Model):
+    nome = models.CharField(max_length=100),
+    tipo = models.CharField(max_length=50),
+    dia_hora = models.DateTimeField(),
+    alerta = models.DateTimeField(),
+    status = models.BooleanField(default=False),
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- med/urls.py
+++ med/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),
     path('homepage_register/', views.homepage_register, name='homepage_register'),
     path('just_register/', views.just_register, name='just_register'),
 ]
