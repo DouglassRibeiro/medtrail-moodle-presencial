346827a056f50d9bcfb6d37d36f1e0fe56dc2331
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2024-12-12 15:37:30 -0300

    return render(request, 'pages/homepage.html')

--- med/views.py
+++ med/views.py
@@ -17,12 +17,13 @@ def registrar_pessoa(request):
         form = PessoaForm(request.POST)
         if form.is_valid():
             pessoa = form.save(commit=False)
-            # Exemplo de como salvar a senha com hash:
+            # salvar a senha com hash:
             from django.contrib.auth.hashers import make_password
             pessoa.senha = make_password(form.cleaned_data['senha'])
             pessoa.save()
             messages.success(request, "Registro realizado com sucesso!")
-            return redirect('pages/homepage.html')  # Altere para o nome da URL desejada
+            return render(request, 'pages/homepage.html')
+            #return redirect('pages/homepage.html')  # Altere para o nome da URL desejada // continua do endereço anterior 
     else:
         form = PessoaForm()
     return render(request, 'pages/register.html', {'form': form})
